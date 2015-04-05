92d8d42e34763f0e0adf7fd920387b70282779df
upbyte <zipmax@gmail.com>
2015-04-05 14:19:30 +0300

    Create first.cpp
    Ваша первая программа на С++

---
+++ first.cpp
@@ -0,0 +1,8 @@
+#include <iostream> 
+using namespace std; 
+int main()
+{ 
+cout << "Hello, world!" << endl; 
+system("pause"); // Только для тех, у кого MS Visual Studio 
+return 0; 
+}</iostream>
